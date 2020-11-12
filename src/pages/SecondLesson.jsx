949b35698b0ef81e77ab3f0c0b3166d3d2ff2547
藤島太智 <fujishimataichi@fujishimataichinoMacBook-puro.local>
2020-11-12 20:43:47 +0900

    2020/11/11

--- src/pages/SecondLesson.jsx
+++ src/pages/SecondLesson.jsx
@@ -1,16 +1,19 @@
-import React from 'react'
-import {useState} from 'react'
+import React, {useState, useContext, useEffect} from 'react'
 import Parent from './components/Parent.jsx'
 import Button from './components/Button.jsx'
+import Context from './Context.jsx'
 
 const SecondLesson = () => {
     const [color, setColor] = useState("red")
+    const user=useContext(Context)
 
     return (
         <div className="box">
             <h1>Second Lesson</h1>
             <Parent name="太郎" color={color} />
-            <Button whatColor="green" setColor={setColor} />
+            <Button whatColor="green" setColor={setColor}>
+                ボタンだよ！
+            </Button>
             {/* <Parent/>
             <Parent/> */}
         </div>
